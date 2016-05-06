e19ce6846359128c2e34160a3418eca472ecbace
Joshua Henderson <digitalpeer@digitalpeer.com>
2016-05-05 20:20:42 -0700

    Add release script to build all board configs

---
+++ scripts/build_all_configs.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+EENSY_PATH=../eensy
+VERSION=1.0-rc1
+
+for c in $EENSY_PATH/configs/*_defconfig
+do
+    rm -rf output
+
+    base=$(basename $c)
+    name=$(echo $base | cut -d '_' -f 1)
+    make BR2_EXTERNAL=$EENSY_PATH $base
+    make
+    cd output/images
+    xz sdcard.img
+    mv sdcard.img.xz ../../eensy-$VERSION-$name.img.xz
+    cd -
+done
